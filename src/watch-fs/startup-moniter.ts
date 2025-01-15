f638862099ee55859822d8c85b6e4cd0f3ff7489
lin onetwo <linonetwo012@gmail.com>
2025-01-15 20:45:16 +0800

    fix: don't let watch mode block test execution

--- src/watch-fs/startup-moniter.ts
+++ src/watch-fs/startup-moniter.ts
@@ -12,6 +12,8 @@ exports.synchronous = true;
 exports.startup = () => {
   // Works on NodeJS side and when FileSystemAdaptor is working
   if (typeof $tw === 'undefined' || !$tw?.node || !$tw.syncadaptor) return;
+  // don't let watch mode block test execution
+  if (process.env.TEST === 'true') return;
   const { FileSystemMonitor } = require('$:/plugins/linonetwo/watch-fs/FileSystemMonitor.js');
   const monitor = new FileSystemMonitor();
   ($tw as ITWWithMonitor).watchFs = monitor;
